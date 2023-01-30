b818360f5093b5cd61cbef3e395d4830f51a5a97
Aaron Byrd <aaronbyrd86@thinkful.com>
2023-01-30 12:48:24 -0600

    refactored substitution module

--- src/substitution.js
+++ src/substitution.js
@@ -5,14 +5,11 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
-  function checkUniqueness(alphabet){
-    
+  function checkUniqueness(alphabet) {
     let checkArr = [];
 
-    for(let i = 0; i < alphabet.length; i++){
-      
-      if (checkArr.includes(alphabet[i]))  return false;
-      
+    for (let i = 0; i < alphabet.length; i++) {
+      if (checkArr.includes(alphabet[i])) return false;
 
       checkArr.push(alphabet[i]);
     }
@@ -20,49 +17,46 @@ const substitutionModule = (function () {
     return true;
   }
 
+  function createLookupTable(alphabet){
+    let lookupTable = {}
+    const startOfAlphabet = 97;
+
+    for (let i = 0; i < alphabet.length; i++){
+      const letter = alphabet[i];
+      lookupTable[String.fromCharCode(startOfAlphabet + i)] = letter;
+    }
+
+    return lookupTable;
+  }
+
   function substitution(input, alphabet, encode = true) {
     // your solution code here
 
-    if(!alphabet || alphabet.length != 26 || !checkUniqueness(alphabet)) return false;
+    if (!alphabet || alphabet.length != 26 || !checkUniqueness(alphabet))
+      return false;
 
-    subArr = alphabet.split("")
+    const lookupTable = createLookupTable(alphabet);
+    let message = [];
 
-    let lookupTable = {};
-    const startOfAlphabet = 97;
-    let i = 0;
 
-    for(const letter of subArr){
-      lookupTable[String.fromCharCode(startOfAlphabet + i)] = letter;
-      i++
-    }
+    for (let i = 0; i < input.length; i++){
+      const currentLetter = input[i].toLocaleLowerCase();
 
-    let message = []
-
-    if(encode){
-      for(let i = 0; i < input.length; i++) {
-        const currentLetter = input[i].toLocaleLowerCase()
-        
-        if(currentLetter !== " ")
-          message.push(lookupTable[currentLetter])
-        else
-          message.push(" ")
-      } 
-    }
-
-    else {
-      for (let i = 0; i < input.length; i ++){
-        const currentLetter = input[i].toLocaleLowerCase();
-        
-        if(currentLetter !== " "){
-          const decodedLetter = Object.keys(lookupTable).find(letter => lookupTable[letter] === currentLetter)
+      if(currentLetter === " ") 
+        message.push(" ");
+      else {
+        if(encode) {
+          message.push(lookupTable[currentLetter]);
+        } else {
+          const decodedLetter = Object.keys(lookupTable).find(
+            (letter) => lookupTable[letter] === currentLetter
+          );
           message.push(decodedLetter);
         }
-        else
-          message.push(" ")
       }
     }
 
-    message = message.join("")
+    message = message.join("");
 
     return message;
   }
