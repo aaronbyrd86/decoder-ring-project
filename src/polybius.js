84880e8de00d27ed4cd285ad2db8082dbe2440d1
Aaron Byrd <aaronbyrd86@thinkful.com>
2023-01-30 15:51:20 -0600

    completed encoding for polybius

--- src/polybius.js
+++ src/polybius.js
@@ -8,6 +8,52 @@ const polybiusModule = (function () {
 
   function polybius(input, encode = true) {
     // your solution code here
+    const startOfAlphabet = 97;
+    let lookupTable = {};
+    let codes = [];
+    let message = [];
+
+    for (let i = 0; i < 5; i++) {
+      const codeValue1 = i + 1;
+
+      for (let j = 0; j < 5; j++) {
+        const codeValue2 = j + 1;
+        const code = codeValue2.toString() + codeValue1.toString();
+        codes.push(code);
+      }
+    }
+
+    for (let i = 0; i < 25; i++) {
+      if (i < 8)
+        lookupTable[String.fromCharCode(startOfAlphabet + i)] = codes[i];
+
+      if (i === 8) lookupTable["i/j"] = codes[i];
+
+      if (i > 8) {
+        if (i === 9)
+          lookupTable[String.fromCharCode(startOfAlphabet + i + 1)] = codes[i];
+        else
+          lookupTable[String.fromCharCode(startOfAlphabet + i + 1)] = codes[i];
+      }
+    }
+
+    for (let i = 0; i < input.length; i++){
+      
+      if (encode){
+        const letter = input[i];
+        
+        if(letter === " ")
+          message += " ";
+        else if(letter === "i" || letter === "j")
+          message += lookupTable["i/j"];
+        else
+          message += lookupTable[letter];
+      }
+    }
+    
+
+    return message;
+
   }
 
   return {
