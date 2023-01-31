6dea3fc13552bfa61b2c4ab190c1c27423f4f529
Aaron Byrd <aaronbyrd86@thinkful.com>
2023-01-31 11:48:25 -0600

    uncommented all test files

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,74 +1,74 @@
-// // Write your tests here!
-// const { expect, should } = require("chai");
-// const { polybius } = require("../src/polybius");
+// Write your tests here!
+const { expect, should } = require("chai");
+const { polybius } = require("../src/polybius");
 
-// describe("polybius", () =>{
-//     describe("error handling", () => {
-//         it("should return false if the message to decode is not even", () => {
-//             const input = "44324233521254134";
+describe("polybius", () =>{
+    describe("error handling", () => {
+        it("should return false if the message to decode is not even", () => {
+            const input = "44324233521254134";
 
-//             const actual = polybius(input, false)
+            const actual = polybius(input, false)
 
-//             expect(actual).to.be.false;
-//         });
+            expect(actual).to.be.false;
+        });
 
-//         it("should return false if the message to decode is not even excluding spaces", () => {
-//             const input = "4432 4233521254134";
+        it("should return false if the message to decode is not even excluding spaces", () => {
+            const input = "4432 4233521254134";
 
-//             const actual = polybius(input, false)
+            const actual = polybius(input, false)
 
-//             expect(actual).to.be.false;
-//         });
-//     describe("encoding", () => {
-//         it("should return the encoded message as a string", () => {
-//             const input = "thinkful";
+            expect(actual).to.be.false;
+        });
+    describe("encoding", () => {
+        it("should return the encoded message as a string", () => {
+            const input = "thinkful";
             
-//             const actual = polybius(input)
+            const actual = polybius(input)
 
-//             expect(actual).to.be.string;
-//         });
+            expect(actual).to.be.string;
+        });
         
-//         it("should encode a message", () => {
-//             const input = "thinkful";
-//             const expected = "4432423352125413";
+        it("should encode a message", () => {
+            const input = "thinkful";
+            const expected = "4432423352125413";
             
-//             const actual = polybius(input)
+            const actual = polybius(input)
 
-//             expect(actual).to.equal(expected);
-//             console.log(`encoding: ${input} gave ${actual}`)
-//         });
+            expect(actual).to.equal(expected);
+            console.log(`encoding: ${input} gave ${actual}`)
+        });
 
-//         it("should encode a message with spaces and capital letters", () => {
-//             const input = "Hello world";
-//             const expected = "3251131343 2543241341";
+        it("should encode a message with spaces and capital letters", () => {
+            const input = "Hello world";
+            const expected = "3251131343 2543241341";
             
-//             const actual = polybius(input)
+            const actual = polybius(input)
 
-//             expect(actual).to.equal(expected);
-//             console.log(`encoding: ${input} gave ${actual}`)
-//         });
-//     });
+            expect(actual).to.equal(expected);
+            console.log(`encoding: ${input} gave ${actual}`)
+        });
+    });
     
-//     describe("decoding", () => {
-//         it("should decode a message with spaces", () => {
-//             const input = "3251131343 2543241341";
-//             const expected = "hello world";
+    describe("decoding", () => {
+        it("should decode a message with spaces", () => {
+            const input = "3251131343 2543241341";
+            const expected = "hello world";
 
-//             const actual = polybius(input, false);
+            const actual = polybius(input, false);
 
-//             expect(actual).to.equal(expected);
-//             console.log(`decoding: ${input} gave ${actual}`)
-//         });
+            expect(actual).to.equal(expected);
+            console.log(`decoding: ${input} gave ${actual}`)
+        });
 
-//         it("should decode a message with i or j", () => {
-//             const input = "4432423352125413";
-//             const expected = "thi/jnkful";
+        it("should decode a message with i or j", () => {
+            const input = "4432423352125413";
+            const expected = "thi/jnkful";
 
-//             const actual = polybius(input, false);
+            const actual = polybius(input, false);
 
-//             expect(actual).to.equal(expected);
-//             console.log(`decoding: ${input} gave ${actual}`)
-//         });
-//     })
-//     })
-// })
+            expect(actual).to.equal(expected);
+            console.log(`decoding: ${input} gave ${actual}`)
+        });
+    })
+    })
+})
